fa9222b7d6e8759a040d429ebcbae5a3fddd11bd
Max Mylnikov <mmmylnikov@yandex.ru>
2024-03-05 15:26:26 +0500

    solved level 2 cases a,b

--- challenges/models.py
+++ challenges/models.py
@@ -1,10 +1,97 @@
 from django.db import models
+from django.core import serializers
+from django.contrib.auth.models import User
+import json
 
 
-class Book(models.Model):
+class InstanceToJsonMixin:
+    def to_json(self) -> dict:
+        return json.loads(serializers.serialize(
+            'json', [self], ensure_ascii=False)[1:-1])
+
+
+class Book(InstanceToJsonMixin, models.Model):
     title = models.CharField(max_length=256)
     author_full_name = models.CharField(max_length=256)
     isbn = models.CharField(max_length=10)
 
     def __str__(self):
         return self.title
+
+
+class Laptop(InstanceToJsonMixin, models.Model):
+    brand = models.CharField(
+        max_length=64,
+        choices=[
+            ('apple', 'Apple'),
+            ('hp', 'Hewlett-Packard'),
+            ('acer', 'Acer'),
+            ('lenovo', 'Lenovo'),
+        ],
+        verbose_name='Бренд'
+    )
+    release_year = models.DateField(verbose_name='Бренд')
+    ram = models.PositiveSmallIntegerField(verbose_name='Объем ОЗУ')
+    hdd = models.PositiveSmallIntegerField(verbose_name='Объем HDD')
+    amount = models.FloatField(verbose_name='Цена')
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name='Количество на складе')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Добавлено')
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name='Обновлено')
+
+    def __str__(self):
+        output = self.get_brand_display() + ' '
+        output += str(self.release_year.year) + '/'
+        output += 'RAM ' + str(self.ram) + 'GB/'
+        output += 'HDD ' + str(self.hdd) + 'GB'
+        return output
+
+    class Meta:
+        ordering = ['-release_year']
+        get_latest_by = 'release_year'
+        verbose_name = 'Ноутбук'
+        verbose_name_plural = 'Ноутбуки'
+
+
+class Post(InstanceToJsonMixin, models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT, verbose_name='Автор')
+    status = models.CharField(
+        max_length=9,
+        choices=[
+            ('published', 'опубликован'),
+            ('draft', 'не опубликован'),
+            ('banned', 'забанен'),
+        ],
+        verbose_name='Статус'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Добавлен')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
+    published_at = models.DateTimeField(null=True, verbose_name='Опубликован')
+    category = models.CharField(
+        max_length=4,
+        null=True,
+        choices=[
+            ('home', 'дом'),
+            ('work', 'работа'),
+            ('dev', 'разработка'),
+        ],
+        verbose_name='Категория'
+    )
+
+    def __str__(self):
+        output = self.title + ' '
+        output += '(' + self.get_category_display() + '), '
+        output += self.author.first_name
+        return output
+
+    class Meta:
+        ordering = ['-published_at']
+        get_latest_by = 'published_at'
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
--- challenges/views/level_2/a_laptops.py
+++ challenges/views/level_2/a_laptops.py
@@ -4,14 +4,21 @@
 и дата добавления.
 
 Ваша задача:
-- создать соответствующую модель (в models.py)
-- создать и применить миграцию по созданию модели (миграцию нужно добавить в пул-реквест)
-- заполнить вашу локальную базу несколькими ноутбуками для облегчения тестирования
++ создать соответствующую модель (в models.py)
++ создать и применить миграцию по созданию модели (миграцию нужно добавить в пул-реквест)
++ заполнить вашу локальную базу несколькими ноутбуками для облегчения тестирования
   (я бы советовал использовать для этого shell)
-- реализовать у модели метод to_json, который будет преобразовывать объект ноутбука в json-сериализуемый словарь
-- по очереди реализовать каждую из вьюх в этом файле, проверяя правильность их работу в браузере
++ реализовать у модели метод to_json, который будет преобразовывать объект ноутбука в json-сериализуемый словарь
++ по очереди реализовать каждую из вьюх в этом файле, проверяя правильность их работу в браузере
 """
 from django.http import HttpRequest, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from challenges.models import Laptop
+import json
+
+
+HTTP_RESPONSE_404 = HttpResponse('', status=404)
+HTTP_RESPONSE_403 = HttpResponse('', status=403)
 
 
 def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
@@ -19,7 +26,11 @@ def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
     В этой вьюхе вам нужно вернуть json-описание ноутбука по его id.
     Если такого id нет, вернуть 404.
     """
-    pass
+    try:
+        laptop = Laptop.objects.get(id=laptop_id)
+    except ObjectDoesNotExist:
+        return HTTP_RESPONSE_404
+    return HttpResponse(str(laptop.to_json()), status=200)
 
 
 def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
@@ -27,7 +38,9 @@ def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
     В этой вьюхе вам нужно вернуть json-описание всех ноутбуков, которых на складе больше нуля.
     Отсортируйте ноутбуки по дате добавления, сначала самый новый.
     """
-    pass
+    laptops = Laptop.objects.filter(quantity__gt=0).order_by('-created_at')
+    content = [laptop.to_json() for laptop in laptops]
+    return HttpResponse(str(content), status=200)
 
 
 def laptop_filter_view(request: HttpRequest) -> HttpResponse:
@@ -37,7 +50,28 @@ def laptop_filter_view(request: HttpRequest) -> HttpResponse:
     Если бренд не входит в список доступных у вас на сайте или если цена отрицательная, верните 403.
     Отсортируйте ноутбуки по цене, сначала самый дешевый.
     """
-    pass
+    brand_raw = request.GET.get('brand')
+    min_price_raw = request.GET.get('min_price')
+
+    laptops = Laptop.objects.all()
+
+    if brand_raw:
+        if brand_raw not in [brand for brand, _ in Laptop.brand.field.choices]:
+            return HTTP_RESPONSE_403
+        laptops = laptops.filter(brand=brand_raw)
+
+    if min_price_raw:
+        try:
+            min_price = float(min_price_raw)
+        except ValueError:
+            return HTTP_RESPONSE_403
+        if min_price < 0:
+            return HTTP_RESPONSE_403
+        laptops = laptops.filter(amount__gte=min_price)
+
+    laptops = laptops.order_by('amount')
+    content = [laptop.to_json() for laptop in laptops]
+    return HttpResponse(str(content), status=200)
 
 
 def last_laptop_details_view(request: HttpRequest) -> HttpResponse:
@@ -45,4 +79,7 @@ def last_laptop_details_view(request: HttpRequest) -> HttpResponse:
     В этой вьюхе вам нужно вернуть json-описание последнего созданного ноутбука.
     Если ноутбуков нет вообще, вернуть 404.
     """
-    pass
+    last_laptop = Laptop.objects.order_by('created_at').last()
+    if not last_laptop:
+        return HTTP_RESPONSE_404
+    return HttpResponse(str(last_laptop.to_json()), status=200)
